d51d9e78fbfcbf0e4af577e5a6cbee1ef7adf988
Alamart1683 <Alamart1683@yandex.ru>
2020-08-08 20:56:09 +0300

    Выполнена третья лабораторная работа

---
+++ src/main/java/com/labs/labs/controller/StudyGroupController.java
@@ -0,0 +1,44 @@
+package com.labs.labs.controller;
+
+import com.labs.labs.dao.StudyGroupJdbc;
+import com.labs.labs.model.Student;
+import com.labs.labs.model.StudyGroup;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class StudyGroupController {
+    private final StudyGroupJdbc studyGroupJdbc;
+
+    public StudyGroupController(StudyGroupJdbc studyGroupJdbc) {
+        this.studyGroupJdbc = studyGroupJdbc;
+    }
+
+    @GetMapping("/group/{id}")
+    public StudyGroup getStudent(@PathVariable int id) {
+        StudyGroup studyGroup = studyGroupJdbc.get(id);
+        return studyGroup;
+    }
+
+    @GetMapping("/group/all")
+    public List<StudyGroup> getAllStudent() {
+        List<StudyGroup> studyGroupList = studyGroupJdbc.getAll();
+        return studyGroupList;
+    }
+
+    @PostMapping("/group/add")
+    public String addGroup(@RequestParam("name") String name) {
+        return studyGroupJdbc.add(name);
+    }
+
+    @PutMapping("/group/update")
+    public String updateStudent(@RequestParam("id") int id, @RequestParam("name") String name) {
+        return studyGroupJdbc.update(id, name);
+    }
+
+    @DeleteMapping("/group/delete/{id}")
+    public String deleteStudent ( @PathVariable int id) {
+        return studyGroupJdbc.delete(id);
+    }
+}
