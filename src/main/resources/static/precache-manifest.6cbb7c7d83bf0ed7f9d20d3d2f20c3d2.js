64c179e686757ce4277a2b91ff7603ea59300cc3
Alamart1683 <Alamart1683@yandex.ru>
2020-08-10 15:15:18 +0300

    Загружена четвертая лабораторная работа

---
+++ src/main/resources/static/precache-manifest.6cbb7c7d83bf0ed7f9d20d3d2f20c3d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a2b61b340aaac1135358fc58cba8da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8608927c4aacd78ca053",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "752aa285dbed3eb7741c",
+    "url": "/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "8608927c4aacd78ca053",
+    "url": "/static/js/2.3652d783.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3652d783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "752aa285dbed3eb7741c",
+    "url": "/static/js/main.4b1ed10d.chunk.js"
+  },
+  {
+    "revision": "6438ddffc2b31e007d69",
+    "url": "/static/js/runtime-main.330dec55.js"
+  }
+]);
